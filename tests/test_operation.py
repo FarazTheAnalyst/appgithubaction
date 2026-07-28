45150ac0159caa83bba873a18eb488efaf9fab8d
faraztheanalyst <farazgill12@icloud.com>
2026-07-28 19:06:00 +0500

    update math operations file

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -6,7 +6,7 @@ def test_add():
     assert add(0, 0) == 0
 
 def test_subtract():
-    assert subtract(5, 3) == 2
-    assert subtract(0, 0) == 0
-    assert subtract(-1, -1) == 0
-    assert subtract(10, 5) == 5
+    assert sub(5, 3) == 2
+    assert sub(0, 0) == 0
+    assert sub(-1, -1) == 0
+    assert sub(10, 5) == 5
